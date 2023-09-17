37ce482a9ce58698c1b3fa5fe4d66f274f0373d9
Max <77535533+Max907-dev@users.noreply.github.com>
2023-09-17 19:32:48 +0200

    Add files via upload
    Updated generator. Added the ability to change the length of the login and password

--- generate_password.py
+++ generate_password.py
@@ -3,10 +3,11 @@ import string
 
 total_log = string.ascii_lowercase + string.ascii_uppercase + string.digits
 total_pass = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
-length = 16
+n = int(input('Press length login: '))
+m = int(input('Press length password: '))
 
-login = ''. join((random.sample(total_log, 10)))
-password = ''.join(random.sample(total_pass, length))
+login = ''. join((random.sample(total_log, n)))
+password = ''.join(random.sample(total_pass, m))
 print('login:  {}\nPassword: {}'.format(login, password))
 
 
